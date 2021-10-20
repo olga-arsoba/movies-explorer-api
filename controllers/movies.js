a66165d742787e268d4ec191d11c9d82f79e3f7a
Olga Arsoba <pelykhvolha@gmail.com>
2021-10-20 12:01:30 +0200

    Revert deleteMovie

--- controllers/movies.js
+++ controllers/movies.js
@@ -51,7 +51,7 @@ module.exports.createMovie = (req, res, next) => {
 };
 
 module.exports.deleteMovie = (req, res, next) => {
-  Movie.find(req.params.movieId)
+  Movie.findById(req.params.movieId)
     .then((movie) => {
       if (movie === null) {
         throw new NotFoundError('Фильм с указанным _id не найдена');
